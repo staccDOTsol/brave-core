08276b67f3b2ce4223818e629866c2f1d7e96725
Vadym Struts <vstruts@brave.com>
2026-09-23 22:32:42 +0200

    review
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- browser/ui/webui/psst/brave_psst_dialog_ui.cc
+++ browser/ui/webui/psst/brave_psst_dialog_ui.cc
@@ -102,7 +102,6 @@ void BravePsstDialogUI::Close() {
   if (!desktop_dialog_delegate_) {
     return;
   }
-
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(
